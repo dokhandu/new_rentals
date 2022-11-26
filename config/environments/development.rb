0e613ff15a3fc4d2ce296fd1b4e9f60f1749df1a
Dorji Khandu <dorji.khandu@selise.ch>
2022-11-26 16:44:54 +0600

    Feature: Add LetterOppener as mail delivery method for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,16 +71,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.active_storage.service = :local
-  config.active_storage.service = :local
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"],
-    :authentication => "plain",
-    :renable_starttls_auto => true
-  }
 end
