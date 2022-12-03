6a9ab37db43070fae567d913e01e7beb38c2ab50
Dorji Khandu <dorji.khandu@selise.ch>
2022-12-03 15:42:53 +0600

    Update: LOG Final Update

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
+  before_action :set_logo
   default from: ENV.fetch('GMAIL_USERNAME', nil)
   layout 'mailer'
+
+
+
+  def set_logo
+    attachments.inline['logo_upc.png'] = Rails.root.join('app/assets/img.png').read
+  end
 end
